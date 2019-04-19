a94bb298cd9680c3757f732d0bd2f5e22f8e6c0e
Johan Siau <johansiautama@gmail.com>
2019-04-19 22:05:25 +0700

    Update babel.config.js

--- src/client/babel.config.js
+++ src/client/babel.config.js
@@ -2,15 +2,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // plugins: [
-    //   [
-    //     'react-native-web',
-    //     {
-    //       commonjs: true,
-    //     },
-    //   ],
-    //   'add-module-exports',
-    //   'transform-object-assign',
-    // ],
+    plugins: [
+      [
+        'react-native-web',
+        {
+          commonjs: true,
+        },
+      ],
+      'add-module-exports',
+      'transform-object-assign',
+    ],
   };
 };
